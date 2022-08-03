172eee17ed6a55e7311e87b4e5afaee788df1f11
Miniling <bsm3925@naver.com>
2022-08-04 01:10:29 +0900

    Modify/LoginPage

--- healthhub/src/pages/LoginPage.js
+++ healthhub/src/pages/LoginPage.js
@@ -1,19 +1,30 @@
+import { useNavigate } from 'react-router-dom';
 import ProfileContainer from '../components/background/ProfileBG';
 import SignupHeader from '../components/SignupHeader';
 import LoginForm from '../forms/LoginForm';
 import '../styles/pages/LoginPage.css';
 
 function LoginPage() {
+  const navigate = useNavigate();
+
+  const moveSignupPage = () => {
+    navigate(`/signup`);
+  }
+
   return (
     <ProfileContainer>
       <div className="LoginPage">
         <SignupHeader />
         <div className="login_form">
           <LoginForm />
-          안나오나
         </div>
         <div className="login_bottom">
-          <div>Sign Up</div>
+          <button
+            className="login_bottom_button"
+            onClick={() => { moveSignupPage() }}
+          >
+            Signup
+          </button>
         </div>
       </div>
     </ProfileContainer>
