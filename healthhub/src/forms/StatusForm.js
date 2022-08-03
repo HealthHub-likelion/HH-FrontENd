8bec34c84562f54e47970371d16efd54baef8199
Miniling <bsm3925@naver.com>
2022-08-04 00:51:07 +0900

    Merge feature/SettingsSession

---
+++ healthhub/src/forms/StatusForm.js
@@ -0,0 +1,30 @@
+import '../styles/forms/StatusForm.css';
+
+function StatusForm() {
+
+    return (
+        <div className="StatusForm">
+            <div className="statusform_title">
+                Status
+            </div>
+            <div className='statusform_button'>
+                <button
+                    type="button"
+                    // onClick={()=>}
+                    className="statusform_button_logout"
+                >
+                    로그아웃
+                </button>
+                <button
+                    type="button"
+                    // onClick={()=>}
+                    className="statusform_button_deleteaccount">
+                    회원탈퇴
+                </button>
+            </div>
+        </div>
+
+    )
+}
+
+export default StatusForm;
