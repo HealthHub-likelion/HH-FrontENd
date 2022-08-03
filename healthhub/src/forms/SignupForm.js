8de4148bd410e6d03bc11b79eb512b3a846113fe
OhSeChan <seb_s19@naver.com>
2022-08-03 18:41:56 +0900

    Add Signup

---
+++ healthhub/src/forms/SignupForm.js
@@ -0,0 +1,32 @@
+import '../styles/forms/SignupForm.css'
+import { useNavigate } from 'react-router-dom';
+
+function SignupForm() {
+    const navigate = useNavigate();
+
+    return (
+        <div className="SignupForm">
+            <div className='signupForm_nickname'>
+                <input 
+                    placeholder='닉네임'>
+                </input>
+                <div>확인</div>
+            </div>
+            <div className='signupForm_email'>
+                <input placeholder='이메일'></input>
+                <div>확인</div>
+            </div>
+            <input 
+                placeholder='password'
+                className='singupForm_password'
+                ></input>
+            <input 
+                placeholder='password 중복 검사'
+                className='signupForm_doubleCheck'
+                ></input>
+            <div className='signupForm_signup'>Signup</div>
+        </div>
+    );
+}
+
+    export default SignupForm;
--- healthhub/src/pages/SignupPage.js
+++ healthhub/src/pages/SignupPage.js
@@ -1,8 +1,22 @@
+import React from "react";
+import IndexContainer from "../components/background/indexBG";
+import SignupForm from "../forms/SignupForm";
+import SignupHeader from "../components/SignupHeader";
+import '../styles/pages/SignupPage.css'
+
 function SignupPage() {
     return (
-      <div className="SignupPage">
-        SignupPage
-      </div>
+      <IndexContainer>
+        <div className="SignupPage">
+          <SignupHeader/>
+          <div className="signup_form">
+            <SignupForm/>
+          </div>
+          <div className="signup_bottom">
+            <div>Login</div>
+          </div>
+        </div>
+      </IndexContainer>
     );
   }
 
