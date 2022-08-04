ef0ff92e030c9c78119cc7f1ec6579be07e9de58
OhSeChan <seb_s19@naver.com>
2022-08-04 13:56:56 +0900

    Add WaveElement

--- healthhub/src/sessions/RecordsSession.js
+++ healthhub/src/sessions/RecordsSession.js
@@ -1,19 +1,22 @@
 import '../styles/sessions/RecordSession.css';
 import RecordsState from '../components/RecordsState';
+import WaveElement from '../components/WaveElement';
 
 function RecordSession() {
     return (
         <div className = 'RecordSession'>
-            <div className='recordsState'>
+            <div className='recordSession_recordsState'>
                 <RecordsState/>
             </div>
-            <div className='wavesWindow'>
+            <div className='recordSession_wavesWindow'>
                 <div className='wavesWindow_header'>
                     <div className = 'wavesWindow_header_title'>WAVES</div>
                     <div className = 'wavesWindow_header_recordsCnt'>24 records</div>
                 </div>
                 <div className = 'wavesWindow_content'>
-
+                    <WaveElement/>
+                    <WaveElement/>
+                    <WaveElement/>
                 </div>
             </div>
         </div>
