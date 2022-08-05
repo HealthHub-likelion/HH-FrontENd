563a4f9be9784bd5bfbb4e16c71b673f2e0dbdae
GuHyeonWoo <ghw9174@gmail.com>
2022-08-05 21:31:27 +0900

    set modals

---
+++ healthhub/src/components/modals/CreateRoutineModal.js
@@ -0,0 +1,12 @@
+import '../../styles/components/modals/CreateRoutineModal.css'
+
+function CreateRoutineModal() {
+
+    return (
+        <div className="CreateRoutineModal">
+            
+        </div>
+    );
+}
+
+export default CreateRoutineModal;
