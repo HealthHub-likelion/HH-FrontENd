8a7853e868d30f9a0051edc9e116350b8578d171
so0y <jenny1717@naver.com>
2022-08-07 21:38:18 +0900

    fix FollowSession

--- healthhub/src/sessions/FollowsSession.js
+++ healthhub/src/sessions/FollowsSession.js
@@ -3,9 +3,9 @@ import { useNavigate, useParams } from 'react-router-dom';
 import '../styles/sessions/FollowsSession.css'
 import FollowList from './FollowList';
 
-function FollowsSession() {
+function FollowsSession({showFollowers, setShowFollowers, showFollowings, setShowFollowings}) {
 
-    //임시 팔로우 데이터
+    //임시 팔로워 데이터
     const [data, setData] = useState([{
       name: '구현우',
       id: 'GHWooo',
@@ -31,10 +31,37 @@ function FollowsSession() {
       followed: true
     }]);
 
+    //임시 팔로잉 데이터
+    const [following, setFollowing] = useState([{
+      name: 'Jeon_minGyu',
+      id: 'DrHagha',
+      src: 'https://avatars.githubusercontent.com/u/77195428?v=4'
+    },
+    {
+      name: 'Yong Hyun Jeon',
+      id: 'Raccooon98',
+      src: 'https://avatars.githubusercontent.com/u/101920006?v=4',
+      followed: false
+    },
+    {
+      name: '김성은',
+      id: 'star-sil',
+      src: 'https://avatars.githubusercontent.com/u/70811575?v=4',
+      followed: false
+    },{
+      name: '최영민',
+      id: 'youngmin1006',
+      src: 'https://avatars.githubusercontent.com/u/67896917?v=4',
+      followed: false
+    }
+    ]);
+
+  
     
   return (
     <div className='FollowsSession'>
-      <FollowList data={data} setData={setData}/>
+      <FollowList data={data} setData={setData} showFollowers={showFollowers} setShowFollowers={setShowFollowers}
+      following={following} setFollowing={setFollowing}/>
     </div>
   )
 }
