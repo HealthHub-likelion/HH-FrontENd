cb5b8acc16bbfded64c5620c5d2d39522f7294d0
Miniling <bsm3925@naver.com>
2022-08-08 01:09:42 +0900

    Modify Settings css/Add NickNameForm Func

--- healthhub/src/forms/NickNameForm.js
+++ healthhub/src/forms/NickNameForm.js
@@ -1,17 +1,40 @@
+import { useRef, useState } from 'react';
 import '../styles/forms/NickNameForm.css';
 
 function NickNameForm({ username }) {
+    const textareaRef = useRef();
+
+    const [nicknameContent, setNickNameContent] = useState('');
+
+    const saveUpdate = () => {
+        if (nicknameContent === '') {
+            textareaRef.current.focus();
+            return;
+        }
+        else {
+            if (window.confirm('변경하시겠습니까?')) {
+                window.location.replace(`/${nicknameContent}/Settings`);
+            }
+        }
+    }
+
+    const onNickNameChange = (event) => {
+        setNickNameContent(event.currentTarget.value);
+    };
 
     return (
         <div className="NickNameForm">
             <div className="nicknameform_title">
-                NickName
+                <div>NickName</div>
             </div>
             <div className='nicknameform_nickname'>
                 <input
+                    ref={textareaRef}
+                    onChange={onNickNameChange}
+                    value={nicknameContent}
                     placeholder={username}>
                 </input>
-                <button>확인</button>
+                <button onClick={() => saveUpdate()}>확인</button>
             </div>
         </div>
     )
