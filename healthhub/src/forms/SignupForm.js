620fb7ea73efdd0175cd2de8053a8592c0630a6b
GuHyeonWoo <ghw9174@gmail.com>
2022-08-08 18:39:33 +0900

    add signup axios

--- healthhub/src/forms/SignupForm.js
+++ healthhub/src/forms/SignupForm.js
@@ -1,6 +1,8 @@
 import '../styles/forms/SignupForm.css'
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import proxy from '../security/Proxy.json';
 
 function SignupForm() {
     const navigate = useNavigate();
@@ -9,9 +11,14 @@ function SignupForm() {
     const [password,setPassword] = useState('');
     const [password2,setPassword2] = useState('');
     const [passwordMatch,setPasswordMatch] = useState(false);
+
+    const [nickName, setNickName] = useState('');
+    const [email, setEamil] = useState('');
     
     const handlePassword = (e) => {setPassword(e.target.value)}
     const handlePassword2 = (e) => {setPassword2(e.target.value)}
+    const inputName = (e) =>{setNickName(e.target.value);}
+    const inputEmail = (e) =>{setEamil(e.target.value);}
 
     useEffect(()=>{
         if(password2.length == 0){
@@ -30,17 +37,53 @@ function SignupForm() {
         }
     },[password,password2])
 
+    const axiosNickName = () =>{
+        axios.post(`${proxy['proxy_url']}/accounts/member/`, {
+            name: nickName
+        })
+        .then((res)=>{
+            console.log(res);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    }
+    const axiosEmail = () =>{
+        axios.post(`${proxy['proxy_url']}/accounts/member/`, {
+            email: email
+        }).
+        then((res)=>{
+            console.log(res);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    }
+    const saveUser = () =>{
+        axios.post(`${proxy['proxy_url']}/accounts/`, {
+            name: nickName,
+            email: email,
+            password: password2
+        }).
+        then((res)=>{
+            console.log(res);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    }
+
     return (
         <div className="SignupForm">
             <div className='signupForm_nickname'>
                 <input 
-                    placeholder='닉네임'>
+                    placeholder='닉네임' value={nickName} onChange={inputName}>
                 </input>
-                <div>확인</div>
+                <div onClick={()=>{axiosNickName()}}>확인</div>
             </div>
             <div className='signupForm_email'>
-                <input placeholder='이메일'></input>
-                <div>확인</div>
+                <input placeholder='이메일' value={email} onChange={inputEmail}></input>
+                <div onClick={()=>{axiosEmail()}}>확인</div>
             </div>
             <input 
                 type = "password"
@@ -57,7 +100,7 @@ function SignupForm() {
             <div className='signupForm_passwordAlertBox'>
                 <div className='signupForm_passwordAlert'>{alertComment}</div>
             </div>
-            <div className='signupForm_signup'>Signup</div>
+            <div className='signupForm_signup' onClick={()=>{saveUser()}}>Signup</div>
         </div>
     );
 }
