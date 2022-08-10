10b178234bf653369d3feb393c0b17f7df8f9501
OhSeChan <seb_s19@naver.com>
2022-08-10 21:37:40 +0900

    Add recordsFeeds Img axios fix Signup

--- healthhub/src/components/ElementProfileBox.js
+++ healthhub/src/components/ElementProfileBox.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import '../styles/components/ElementProfileBox.css'
 import { useLocation } from 'react-router-dom';
+import proxy from '../security/Proxy.json';
 
 const ElementProfileBox = ({member_nickname, member_img}) => {
 
     const location = useLocation(); ///userPage/Records or Feeds
     const pageLocation = location.pathname.split("/")[2]; //Records or Feeds
     
+    console.log(member_img);
+
     return (
         <>
             {pageLocation === "Feeds" ?
                 <div className='ElementProfileBox'>
-                    <div className='waveElement_profileImg'><img src = {member_img}/></div>
+                    <div className='waveElement_profileImgBox'>
+                        <img 
+                            className='waveElement_profileImgBox'
+                            src = {`${proxy['proxy_url']}/${member_img}`}/>
+                    </div>
                     <div className='waveElement_profileName'>{member_nickname}</div>
                 </div>
             :null
