196a807456c7c54eb5cc56df0183588617af13c8
GuHyeonWoo <ghw9174@gmail.com>
2022-08-14 06:09:37 +0900

    set userpage data

---
+++ healthhub/src/components/InitialData.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import proxy from '../security/Proxy.json'
+import { useEffect } from 'react';
+
+function InitialData({username, setUserData}) {
+    useEffect(()=>{
+        const token = localStorage.getItem('HH_token')?localStorage.getItem('HH_token'):''
+  
+        axios.get(`${proxy['proxy_url']}/accounts/member/?name=${username}`, {
+          headers:{
+              Authorization: token
+          }
+      })
+      .then((res)=>{
+        const data = res.data;
+
+        if(data.isFollow === null){
+          localStorage.setItem('HH_member_id', data.id);
+        }
+        setUserData(data);
+      })
+      .catch((err)=>{
+        console.log(err);
+        // 후에 사용자를 못찾았다는 경고 페이지 생성
+        // navigate(`/`);
+      })
+      }, [])
+}
+
+export default InitialData;
--- healthhub/src/pages/UserPage.js
+++ healthhub/src/pages/UserPage.js
@@ -1,12 +1,16 @@
-import React from "react";
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from 'react-router-dom';
 import '../styles/pages/UserPage.css'
 import Header from "../components/Header";
 import UserSession from '../sessions/UserSession';
 import ProfileSession from "../sessions/ProfileSession";
+import InitialData from "../components/InitialData";
 
 function UserPage() {
     const {username} = useParams();
+    const [userData, setUserData] = useState({});
+
+    const navigate = useNavigate();
     
     return (
       <div className="UserPage">
@@ -15,12 +19,17 @@ function UserPage() {
         </div>
         <div className="userpage_content">
           <div className="userpage_profile">
-            <ProfileSession username={username}/>
+            <ProfileSession username={username} userData={userData}/>
           </div>
           <div className="userpage_UserSession">
             <UserSession/>
           </div>
         </div>
+
+        <InitialData 
+          username={username}
+          setUserData={setUserData}
+        />
       </div>
     );
   }
