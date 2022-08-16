e307e7e5409c336565678cd6a911b0e0475f7e3e
GuHyeonWoo <ghw9174@gmail.com>
2022-08-17 00:11:05 +0900

    update usersession

--- healthhub/src/sessions/UserSession.js
+++ healthhub/src/sessions/UserSession.js
@@ -2,19 +2,27 @@ import '../styles/sessions/UserSession.css';
 import ReadMeElement from '../components/elements/ReadMeElement';
 import RoutinesElement from '../components/elements/RoutinesElement';
 import WavesElement from '../components/elements/WavesElements';
+import proxy from '../security/Proxy.json';
 
 function UserSession({userData, setUserData}) {
     return (
       <div className="UserSession">
-        <div className='usersession_waves'>
-          <WavesElement userData={userData}/>
+        {userData['isFollow']!==null && userData['isOpen']===false
+        ?<div className='usersession_private'>
+          <img alt='잠금' src={`${proxy['proxy_url']}/media/images/icons/HH_icon_security.png`}/>
+          <div>private user</div>
         </div>
-        <div className='usersession_readme'>
-          <ReadMeElement userData={userData} setUserData={setUserData}/>
-        </div>
-        <div className='usersession_routines'>
-          <RoutinesElement userData={userData} setUserData={setUserData}/>
-        </div>
+        :<>
+          <div className='usersession_waves'>
+            <WavesElement userData={userData}/>
+          </div>
+          <div className='usersession_readme'>
+            <ReadMeElement userData={userData} setUserData={setUserData}/>
+          </div>
+          <div className='usersession_routines'>
+            <RoutinesElement userData={userData} setUserData={setUserData}/>
+          </div>
+        </>}
       </div>
     );
   }
