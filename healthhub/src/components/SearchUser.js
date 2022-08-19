765f9edc16a218a47f15b6fcf9352fc4328c7688
Miniling <bsm3925@naver.com>
2022-08-19 19:54:01 +0900

    Modify RoutineBoxStyle

--- healthhub/src/components/SearchUser.js
+++ healthhub/src/components/SearchUser.js
@@ -13,53 +13,53 @@ const SearchUser = () => {
 
     const [inputName, setInputName] = useState('');
     const [showDropDown, setShowDropDown] = useState(false);
-    const [userList,setUserList] = useState([]);
+    const [userList, setUserList] = useState([]);
 
-    const searchName = (e) =>{
+    const searchName = (e) => {
         setInputName(e.target.value);
     }
 
-    useEffect(()=>{
-        if(inputName!==''){
+    useEffect(() => {
+        if (inputName !== '') {
             setShowDropDown(true);
         }
         else setShowDropDown(false);
     })
 
-    useEffect(()=>{
-    if(inputName !== ''){
-        axios.post(`${process.env.REACT_APP_PROXY}/accounts/membersearchbykeyword`,{
-            keyword : inputName
-        },{
-            headers:{
-                Authorization: token
-            }
-        })
-        .then((res)=>{
-            if(inputName == ''){
-                setUserList([]);
-            }
-            setUserList([res.data.Member][0]);
-        })
-        .catch((err)=>{
-            console.log(err);
+    useEffect(() => {
+        if (inputName !== '') {
+            axios.post(`${process.env.REACT_APP_PROXY}/accounts/membersearchbykeyword`, {
+                keyword: inputName
+            }, {
+                headers: {
+                    Authorization: token
+                }
+            })
+                .then((res) => {
+                    if (inputName == '') {
+                        setUserList([]);
+                    }
+                    setUserList([res.data.Member][0]);
+                })
+                .catch((err) => {
+                    console.log(err);
+                    setUserList([]);
+                })
+        }
+        else {
             setUserList([]);
-        })
-    }
-    else{
-        setUserList([]);
-    }
-    },[inputName]);
+        }
+    }, [inputName]);
 
-    function moveToUser(userName){
+    function moveToUser(userName) {
         navigate(`/${userName}`)
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         const container = document.querySelector('.SearchUser_top_search_dropdown');
         const serarchInput = document.querySelector('.SearchUser_top_search_input');
-        document.addEventListener('mouseup', function(e) {
-            if(container !== null){
+        document.addEventListener('mouseup', function (e) {
+            if (container !== null) {
                 if (!container.contains(e.target)) {
                     container.style.display = 'none';
                 }
@@ -73,29 +73,29 @@ const SearchUser = () => {
     return (
         <div className='SearchUser_top_box'>
             <div className='SearchUser_top_searchBox'>
-                <input className='SearchUser_top_search_input' ref={inputRef} 
-                value={inputName} onChange={searchName} placeholder='user name'/>
-                <img alt='검색' src={`${process.env.REACT_APP_PROXY}/media/images/HH_icon_search_name.png`}/>
+                <input className='SearchUser_top_search_input' ref={inputRef}
+                    value={inputName} onChange={searchName} placeholder='user name' />
+                <img alt='검색' src={`${process.env.REACT_APP_PROXY}/media/images/HH_icon_search_name.png`} />
             </div>
-            {showDropDown && 
-            <div className='SearchUser_top_search_dropdown'>                  
-                {
-                    userList.map((e,i)=>{
-                        return(
-                            <div className="SearchUser_userList" key = {i}
-                                onClick={()=>{moveToUser(e.name)}}>  
-                                <div className="SearchUser_userImgContainer">
-                                    <img
-                                        className="SearchUser_userImg"
-                                        src = {`${process.env.REACT_APP_PROXY}/media/${e.img}`}
-                                    />
+            {showDropDown &&
+                <div className='SearchUser_top_search_dropdown'>
+                    {
+                        userList.map((e, i) => {
+                            return (
+                                <div className="SearchUser_userList" key={i}
+                                    onClick={() => { moveToUser(e.name) }}>
+                                    <div className="SearchUser_userImgContainer">
+                                        <img
+                                            className="SearchUser_userImg"
+                                            src={`${process.env.REACT_APP_IMAGE}/media/${e.img}`}
+                                        />
+                                    </div>
+                                    <div className="SearchUser_userName">{e.name}</div>
                                 </div>
-                                <div className="SearchUser_userName">{e.name}</div>
-                            </div>
-                        )
-                    })
-                }
-            </div>}
+                            )
+                        })
+                    }
+                </div>}
         </div>
     );
 };
