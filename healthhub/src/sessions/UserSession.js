ad8777aae564d484dcd53a5e3cedd616abd22e7d
GuHyeonWoo <ghw9174@gmail.com>
2022-08-19 16:07:32 +0900

    fix

--- healthhub/src/sessions/UserSession.js
+++ healthhub/src/sessions/UserSession.js
@@ -4,26 +4,26 @@ import RoutinesElement from '../components/elements/RoutinesElement';
 import WavesElement from '../components/elements/WavesElements';
 
 function UserSession({userData, setUserData}) {
-    // return (
-    //   <div className="UserSession">
-    //     {userData['isFollow']!==null && userData['isOpen']===false
-    //     ?<div className='usersession_private'>
-    //       <img alt='잠금' src={`${process.env.REACT_APP_PROXY}/media/images/icons/HH_icon_security.png`}/>
-    //       <div>private user</div>
-    //     </div>
-    //     :<>
-    //       <div className='usersession_waves'>
-    //         <WavesElement userData={userData}/>
-    //       </div>
-    //       <div className='usersession_readme'>
-    //         <ReadMeElement userData={userData} setUserData={setUserData}/>
-    //       </div>
-    //       <div className='usersession_routines'>
-    //         <RoutinesElement userData={userData} setUserData={setUserData}/>
-    //       </div>
-    //     </>}
-    //   </div>
-    // );
+    return (
+      <div className="UserSession">
+        {userData['isFollow']!==null && userData['isOpen']===false
+        ?<div className='usersession_private'>
+          <img alt='잠금' src={`${process.env.REACT_APP_PROXY}/media/images/icons/HH_icon_security.png`}/>
+          <div>private user</div>
+        </div>
+        :<>
+          <div className='usersession_waves'>
+            <WavesElement userData={userData}/>
+          </div>
+          <div className='usersession_readme'>
+            <ReadMeElement userData={userData} setUserData={setUserData}/>
+          </div>
+          <div className='usersession_routines'>
+            <RoutinesElement userData={userData} setUserData={setUserData}/>
+          </div>
+        </>}
+      </div>
+    );
   }
 
   export default UserSession;
