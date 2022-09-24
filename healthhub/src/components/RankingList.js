30dcffbd202e17107e76cdf6e11e271bc46da7cf
so0y <jenny1717@naver.com>
2022-09-24 16:19:12 +0900

    add ranking

---
+++ healthhub/src/components/RankingList.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function RankingList() {
+  return (
+    <div className='RankingElement'>
+      <div className='RankingImg'>
+        <img className='profileImgrank'>
+
+        </img>
+      </div>
+      <div className='RankingName'>
+
+      </div>
+      <div className='Ranking'>
+
+      </div>
+    </div>
+  )
+}
+
+export default RankingList
