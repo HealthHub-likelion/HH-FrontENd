bfefa905e6f255d32dbc3473a492d13f4d6d73de
Miniling <bsm3925@naver.com>
2022-09-29 19:30:21 +0900

    Add replyModal

--- healthhub/src/sessions/FeedsSession.js
+++ healthhub/src/sessions/FeedsSession.js
@@ -4,38 +4,38 @@ import '../styles/sessions/FeedsSession.css'
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
-const FeedsSession = ({userData, setUserData}) => {
+const FeedsSession = ({ userData, setUserData }) => {
 
-    const [feed_myList,setMyList] = useState([]);
+    const [feed_myList, setMyList] = useState([]);
     const token = localStorage.getItem('HH_token');
 
-    const axiosFeeds = () =>{
-        axios.get(`${process.env.REACT_APP_PROXY}/record/followinglist/`,{
-            headers:{
-                Authorization : token
+    const axiosFeeds = () => {
+        axios.get(`${process.env.REACT_APP_PROXY}/record/followinglist/`, {
+            headers: {
+                Authorization: token
             }
         })
-        .then((res)=>{
-            // console.log(res);
-            setMyList(res.data);
-        })
-        .catch((err)=>{
-            // console.log(err);
-        })
+            .then((res) => {
+                // console.log(res);
+                setMyList(res.data);
+            })
+            .catch((err) => {
+                // console.log(err);
+            })
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         axiosFeeds();
-    },[]);
+    }, []);
 
-    function getDate(create_time){
-        const getCreate_time = create_time.substr(0,10);
+    function getDate(create_time) {
+        const getCreate_time = create_time.substr(0, 10);
         return getCreate_time;
     }
 
-    function latest(feed_myList){
+    function latest(feed_myList) {
         const latestList = [...feed_myList];
-        latestList.sort((a,b)=>{
+        latestList.sort((a, b) => {
             return b.record_id - a.record_id;
         })
         return latestList;
@@ -43,26 +43,27 @@ const FeedsSession = ({userData, setUserData}) => {
 
     return (
         <div className='FeedsSession'>
-            {feed_myList.length>0?
-                latest(feed_myList).map((e,i)=>{
-                    return(
+            {feed_myList.length > 0 ?
+                latest(feed_myList).map((e, i) => {
+                    return (
                         <WaveElement
-                        userData={userData} 
-                        setUserData={setUserData}
-                        key = {i}
-                        record_img = {e.record_img}
-                        routine_name = {e.routine_name}
-                        create_time = {getDate(e.record_create_time)}
-                        comment = {e.record_comment}
-                        member_nickname = {e.member_nickname}
-                        member_img = {e.member_img}
-                        isOpen={e.routine_isOpen}
-                        routineId = {e.routine_id}
-                        pre='wave'
+                            userData={userData}
+                            setUserData={setUserData}
+                            key={i}
+                            record_id={e.record_id}
+                            record_img={e.record_img}
+                            routine_name={e.routine_name}
+                            create_time={getDate(e.record_create_time)}
+                            comment={e.record_comment}
+                            member_nickname={e.member_nickname}
+                            member_img={e.member_img}
+                            isOpen={e.routine_isOpen}
+                            routineId={e.routine_id}
+                            pre='wave'
                         />
                     )
                 })
-                :<div className='feed_empty'>기록을 공유할 친구를 찾아보세요!</div>
+                : <div className='feed_empty'>기록을 공유할 친구를 찾아보세요!</div>
             }
         </div>
     );
--- healthhub/src/sessions/RecordsSession.js
+++ healthhub/src/sessions/RecordsSession.js
@@ -5,93 +5,94 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import SmallSliceContainer from '../components/background/SmallSliceBG';
 
-function RecordSession({userData, setUserData}) {
+function RecordSession({ userData, setUserData }) {
 
-    const [myList,setMyList] = useState([]);
-    const [consecutiveDay,setConsecutiveDay] = useState(0);
+    const [myList, setMyList] = useState([]);
+    const [consecutiveDay, setConsecutiveDay] = useState(0);
     const token = localStorage.getItem('HH_token');
 
-    const axiosRecords = () =>{
-        axios.get(`${process.env.REACT_APP_PROXY}/record/mylist/`,{
-            headers:{
-                Authorization : token
+    const axiosRecords = () => {
+        axios.get(`${process.env.REACT_APP_PROXY}/record/mylist/`, {
+            headers: {
+                Authorization: token
             }
         })
-        .then((res)=>{
-            // console.log(res);
-            setMyList(res.data);
-        })
-        .catch((err)=>{
-            // console.log(err);
-        })
+            .then((res) => {
+                // console.log(res);
+                setMyList(res.data);
+            })
+            .catch((err) => {
+                // console.log(err);
+            })
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         axiosRecords();
-    },[]);
+    }, []);
 
-    function getDate(create_time){
-        return create_time.substr(0,10);
+    function getDate(create_time) {
+        return create_time.substr(0, 10);
     }
 
-    function getConsecutive(){
+    function getConsecutive() {
         let count = 0;
         const offset = new Date().getTimezoneOffset() * 60000;
-        const today = new Date(Date.now() - offset);    
-        let getToday = new Date(today.toISOString().substring(0,10) + " 00:00:00");
-        latest(myList).map((e)=>{
-            if(Math.ceil((getToday.getTime() - new Date(e.record_create_time.substring(0,10) + " 00:00:00").getTime())/(1000 * 3600 * 24)) === 1){
-                today.setDate(today.getDate()-1);
-                getToday = new Date(today.toISOString().substring(0,10) + " 00:00:00");
+        const today = new Date(Date.now() - offset);
+        let getToday = new Date(today.toISOString().substring(0, 10) + " 00:00:00");
+        latest(myList).map((e) => {
+            if (Math.ceil((getToday.getTime() - new Date(e.record_create_time.substring(0, 10) + " 00:00:00").getTime()) / (1000 * 3600 * 24)) === 1) {
+                today.setDate(today.getDate() - 1);
+                getToday = new Date(today.toISOString().substring(0, 10) + " 00:00:00");
                 count += 1;
             }
         })
         return count;
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         setConsecutiveDay(getConsecutive());
     })
 
-    function latest(feed_myList){
+    function latest(feed_myList) {
         const latestList = [...feed_myList];
-        latestList.sort((a,b)=>{
+        latestList.sort((a, b) => {
             return b.record_id - a.record_id;
         })
         return latestList;
     }
 
     return (
-        <div className = 'RecordSession'>
+        <div className='RecordSession'>
             <div className='recordSession_recordsState'>
                 <SmallSliceContainer>
-                    <RecordsState entireWave = {myList.length} getConsecutive = {consecutiveDay}/>
+                    <RecordsState entireWave={myList.length} getConsecutive={consecutiveDay} />
                 </SmallSliceContainer>
             </div>
             <div className='recordSession_wavesWindow'>
                 <div className='wavesWindow_header'>
-                    <div className = 'wavesWindow_header_title'>WAVES</div>
-                    <div className = 'wavesWindow_header_recordsCnt'>{myList.length} records</div>
+                    <div className='wavesWindow_header_title'>WAVES</div>
+                    <div className='wavesWindow_header_recordsCnt'>{myList.length} records</div>
                 </div>
-                <div className = 'wavesWindow_content'>
-                    {myList.length > 0?
-                        latest(myList).map((e,i)=>{
-                            return(
-                                <WaveElement key = {i}
-                                userData={userData} 
-                                setUserData={setUserData}
-                                record_img = {e.record_img}
-                                routine_name = {e.routine_name}
-                                create_time = {getDate(e.record_create_time)}
-                                comment = {e.record_comment}
-                                member_nickname = {e.member_nickname}
-                                member_img = {e.member_img}
-                                isOpen={e.routine_isOpen}
-                                routineId = {e.routine_id}
-                                pre='wave'
+                <div className='wavesWindow_content'>
+                    {myList.length > 0 ?
+                        latest(myList).map((e, i) => {
+                            return (
+                                <WaveElement key={i}
+                                    userData={userData}
+                                    setUserData={setUserData}
+                                    record_id={e.record_id}
+                                    record_img={e.record_img}
+                                    routine_name={e.routine_name}
+                                    create_time={getDate(e.record_create_time)}
+                                    comment={e.record_comment}
+                                    member_nickname={e.member_nickname}
+                                    member_img={e.member_img}
+                                    isOpen={e.routine_isOpen}
+                                    routineId={e.routine_id}
+                                    pre='wave'
                                 />
                             )
-                        }):
+                        }) :
                         <div className='record_empty'>오늘은 어떤 운동을하셨나요?</div>
                     }
                 </div>
